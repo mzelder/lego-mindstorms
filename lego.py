ef8602ad9ae2948374b871fe848b9dd7259c4c77
oheyek <oheyek@proton.me>
2024-02-27 10:26:01 +0100

    driftowóz

--- lego.py
+++ lego.py
@@ -2,10 +2,13 @@
 
 from time import sleep
 
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_D, SpeedPercent, MoveTank
 from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import TouchSensor
 from ev3dev2.led import Leds
 
-m = LargeMotor(OUTPUT_A)
-m.on_for_rotations(SpeedPercent(75), 5)
+tank_drive = MoveTank(OUTPUT_A, OUTPUT_D)
+
+# drive in a turn for 5 rotations of the outer motor
+# the first two parameters can be unit classes or percentages.
+tank_drive.on_for_rotations(SpeedPercent(0), SpeedPercent(100), 5)
